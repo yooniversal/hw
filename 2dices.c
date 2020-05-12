1ca0150cc83c7f1fa7dd7fb6c6280617df577c42
yooniversal <61930524+yooniversal@users.noreply.github.com>
2020-05-12 11:40:32 +0900

    modif

--- 2dices.c
+++ 2dices.c
@@ -17,27 +17,3 @@ int main(void)
 
 	return 0;
 }
-
-// when use 3 dices, number of cases that add result is 10
-
-#include <stdio.h>
-
-int main(void)
-{
-	int a, b, c;
-
-	for (a = 1; a <= 6; a++)
-	{
-		for (b = 1; b <= 6; b++)
-		{
-			for (c = 1; c <= 6; c++)
-			{
-				if (a + b == 6)
-					printf("(%d, %d, %d)\n", a, b, c);
-			}
-		}
-	}
-
-	return 0;
-}
-
