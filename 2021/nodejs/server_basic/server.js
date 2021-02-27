2cadf374ff15d06209be71d98e35ebb2838c99e7
yooniversal <iteratively@naver.com>
2021-02-28 00:13:33 +0900

    서버 구축하기/내용 수정

--- 2021/nodejs/server_basic/server.js
+++ 2021/nodejs/server_basic/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-//사용자로부터 http 요청이 들어오면 내부 코드가 실행됨
+// 서버 생성 후, 사용자로부터 http 요청이 들어오면 내부 코드가 실행됨
 var server = http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type' : 'text/html'});
     response.end('Hello node.js!');
@@ -10,3 +10,5 @@ var server = http.createServer(function(request, response){
 server.listen(8080, function(){
     console.log('Server is running...');
 });
+
+// http://localhost:8080 에서 메세지가 출력됨을 확인할 수 있음
