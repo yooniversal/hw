ea959a9f5f29f8ed5349a56a866c279f1bad1a31
yooniversal <iteratively@naver.com>
2021-02-28 00:14:15 +0900

    EXPORT TEST

---
+++ 2021/nodejs/server_basic/custom_module.js
@@ -0,0 +1,5 @@
+exports.sum = function(param) {
+    return param*(param+1)/2;
+}
+
+exports.var1 = "test sample";
---
+++ 2021/nodejs/server_basic/home.js
@@ -0,0 +1,4 @@
+var module = require('./custom_module');
+
+console.log('sum(%d) = %d', 100, module.sum(100));
+console.log('var1 = %s', module.var1);
